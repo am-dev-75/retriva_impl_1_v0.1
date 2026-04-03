8292e147c95f79409ed7efdb0f0c0572a98b7cff
Andrea Marson <am.dev.75@gmail.com>
2026-04-03 23:37:28 +0200

    Set local services' URLs.

--- src/retriva/config.py
+++ src/retriva/config.py
@@ -4,12 +4,14 @@ class Settings(BaseSettings):
     mirror_base_path: str = "./mirror"
     canonical_base_url: str = "https://wiki.dave.eu"
     
-    qdrant_url: str = "http://localhost:6333"
+    qdrant_url: str = "http://192.168.1.64:6333"
     openai_api_key: str = "sk-mock-key"
-    openai_base_url: str = "https://api.openai.com/v1"
+    openai_base_url: str = "https://192.168.1.64:8000/v1"
     
-    embedding_model: str = "text-embedding-3-small"
-    chat_model: str = "gpt-4o-mini"
+    #embedding_model: str = "text-embedding-3-small"
+    embedding_model: str = "Qwen/Qwen3-0.6B"
+    #chat_model: str = "gpt-4o-mini"
+    chat_model: str = "Qwen/Qwen3-0.6B"
     
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
